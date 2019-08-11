438562a2440e7b9388d15e08d6c9e656d0c344f9
Cameron Day <32853335+ConanDW@users.noreply.github.com>
2019-08-11 17:49:08 -0400

    fixed bugs

--- getstarted.sh
+++ getstarted.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+echo "Now, please enter the previously setup domain name. Ex: btcpay.bitcoin1776.org"
+read btcpayhost
+sudo su - <<EOF
 mkdir btcpayserver
 alias btcpayserver="cd /btcpayserver"
 cd btcpayserver
 git clone https://github.com/btcpayserver/btcpayserver-docker
 alias btcpayserver-docker="cd /btcpayserver1/btcpayserver-docker"
 cd btcpayserver-docker
-echo "Now, please enter the previously setup domain name. EX: 'btcpay.bitcoin1776.org'"
-read btcpayhost
 export BTCPAY_HOST="$btcpayhost"
+export NBITCOIN_NETWORK="mainnet"
+export BTCPAYGEN_CRYPTO1="btc"
+export BTCPAYGEN_REVERSEPROXY="nginx"
+export BTCPAYGEN_LIGHTNING="lnd"
+export BTCPAYGEN_ADDITIONAL_FRAGMENTS="opt-save-storage-xs;opt-save-memory"
 echo "When you have the ability to type in the terminal again type exit"
-echo "Then go to $btcpayhost to check if everything is syncing /n
-if there are any issues with the install please contact support."
+echo "Then go to $btcpayhost to check if everything is syncing /n if there are any issues with the install please contact support"
+. ./btcpay-setup.sh -i
+EOF

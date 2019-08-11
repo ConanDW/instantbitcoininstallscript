fec05e97556fd7c932648cca3b3ee7815c3a8fc1
Cameron Day <32853335+ConanDW@users.noreply.github.com>
2019-08-11 16:13:32 -0400

    improved formatting, fixed bugs

--- getstarted.sh
+++ getstarted.sh
@@ -1,14 +1,15 @@
+
 #!/bin/bash
 mkdir btcpayserver
 alias btcpayserver="cd /btcpayserver"
-btcpayserver
+cd btcpayserver
 git clone https://github.com/btcpayserver/btcpayserver-docker
 alias btcpayserver-docker="cd /btcpayserver1/btcpayserver-docker"
+cd btcpayserver-docker
 echo "Now, please enter the previously setup domain name. EX: 'btcpay.bitcoin1776.org'"
 read btcpayhost
 export BTCPAY_HOST="$btcpayhost"
 echo "When you have the ability to type in the terminal again type exit"
-echo "Then go to btcpay.yourdomain.yourdomainsending to check if everything is syncing
-/n
+echo "Then go to $btcpayhost to check if everything is syncing /n
 if there is any issues contact support"
                                             
